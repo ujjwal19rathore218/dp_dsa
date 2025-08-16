da338076088aaab844d8f6e23d86f3a54779c654
ujjwal19rathore218 <ujjwalrathore019@gmail.com>
2025-08-16 23:36:23 +0530

    new approach of O(n)

--- celebrityProb.java
+++ celebrityProb.java
@@ -22,3 +22,30 @@ class Solution {
         
     }
 }
+//O(n) approach
+
+class Solution {
+    public int celebrity(int mat[][]) {
+        // code here
+        //two pointer approach
+        int n = mat.length;
+        int i =0;int j=n-1;
+        while(i<j){
+            if(mat[i][j]==1){
+                i++;//because i is not a celebrity
+            }
+            else if(mat[i][j]==0){
+                j--;
+            }
+        }//verification
+        int candidate = i;
+        for(int k=0;k<n;k++){
+            if(k!=candidate){
+                if(mat[k][candidate]==0 || mat[candidate][k]==1){//candidate row has all 0 othher than itself
+                    return -1;
+                }
+            }
+        }
+        return candidate;
+    }
+}
